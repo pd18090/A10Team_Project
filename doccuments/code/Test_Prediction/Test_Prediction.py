83f1352bc47e01039f75de1b6c07f7a9dc7da65f
lc18166 <45485204+lc18166@users.noreply.github.com>
2019-02-04 16:16:41 +0000

    Create prediction code
    Code for creating predictions of house prices in test.csv, based off the features and house prices in train.csv

---
+++ doccuments/code/Test_Prediction/Test_Prediction.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Majority of code taken from kernel at https://www.kaggle.com/dgawlik/house-prices-eda
+#Modifications by Luke Cotton
+
+#%matplotlib inline
+import csv
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import scipy.stats as st
+import sklearn.linear_model as linear_model
+import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+pd.options.display.max_rows = 1000
+pd.options.display.max_columns = 20
+
+train = pd.read_csv('input/train.csv')
+test = pd.read_csv('input/test.csv')
+
+quantitative = [f for f in train.columns if train.dtypes[f] != 'object']
+quantitative.remove('SalePrice')
+quantitative.remove('Id')
+qualitative = [f for f in train.columns if train.dtypes[f] == 'object']
+
+y = train['SalePrice']
+
+test_normality = lambda x: st.shapiro(x.fillna(0))[1] < 0.01
+normal = pd.DataFrame(train[quantitative])
+normal = normal.apply(test_normality)
+
+f = pd.melt(train, value_vars=quantitative)
+
+for c in qualitative:
+    train[c] = train[c].astype('category')
+    if train[c].isnull().any():
+        train[c] = train[c].cat.add_categories(['MISSING'])
+        train[c] = train[c].fillna('MISSING')
+
+def boxplot(x, y, **kwargs):
+    sns.boxplot(x=x, y=y)
+    x=plt.xticks(rotation=90)
+f = pd.melt(train, id_vars=['SalePrice'], value_vars=qualitative)
+
+def anova(frame):
+    anv = pd.DataFrame()
+    anv['feature'] = qualitative
+    pvals = []
+    for c in qualitative:
+        samples = []
+        for cls in frame[c].unique():
+            s = frame[frame[c] == cls]['SalePrice'].values
+            samples.append(s)
+        pval = st.f_oneway(*samples)[1]
+        pvals.append(pval)
+    anv['pval'] = pvals
+    return anv.sort_values('pval')
+
+a = anova(train)
+a['disparity'] = np.log(1./a['pval'].values)
+
+
+def encode(frame, feature):
+    ordering = pd.DataFrame()
+    ordering['val'] = frame[feature].unique()
+    ordering.index = ordering.val
+    if 'SalePrice' in frame.keys():
+        ordering['spmean'] = frame[[feature, 'SalePrice']].groupby(feature).mean()['SalePrice']
+    else: #Use different value than 'SalePrice' in the 'test' data set since 'SalePrice' doesn't exist in 'test'
+        ordering['spmean'] = frame[[feature, 'LotArea']].groupby(feature).mean()['LotArea']
+        ordering = ordering.sort_values('spmean')
+    ordering['ordering'] = range(1, ordering.shape[0]+1)
+    ordering = ordering['ordering'].to_dict()
+
+    for cat, o in ordering.items():
+        frame.loc[frame[feature] == cat, feature+'_E'] = o
+    
+qual_encoded = []
+for q in qualitative:  
+    encode(train, q)
+    encode(test, q) #Also encode the qualitative attributes in the 'test' data set
+    qual_encoded.append(q+'_E')
+
+def spearman(frame, features):
+    spr = pd.DataFrame()
+    spr['feature'] = features
+    spr['spearman'] = [frame[f].corr(frame['SalePrice'], 'spearman') for f in features]
+    spr = spr.sort_values('spearman')
+    
+features = quantitative + qual_encoded
+spearman(train, features)
+
+
+corr = train[quantitative+['SalePrice']].corr()
+corr = train[qual_encoded+['SalePrice']].corr()
+corr = pd.DataFrame(np.zeros([len(quantitative)+1, len(qual_encoded)+1]), index=quantitative+['SalePrice'], columns=qual_encoded+['SalePrice'])
+        
+
+def pairplot(x, y, **kwargs):
+    ax = plt.gca()
+    ts = pd.DataFrame({'time': x, 'val': y})
+    ts = ts.groupby('time').mean()
+    ts.plot(ax=ax)
+    plt.xticks(rotation=90)
+    
+f = pd.melt(train, id_vars=['SalePrice'], value_vars=quantitative+qual_encoded)
+
+features = quantitative
+
+standard = train[train['SalePrice'] < 200000]
+pricey = train[train['SalePrice'] >= 200000]
+
+diff = pd.DataFrame()
+diff['feature'] = features
+diff['difference'] = [(pricey[f].fillna(0.).mean() - standard[f].fillna(0.).mean())/(standard[f].fillna(0.).mean())
+                      for f in features]
+
+
+features = quantitative + qual_encoded
+model = TSNE(n_components=2, random_state=0, perplexity=50)
+X = train[features].fillna(0.).values
+tsne = model.fit_transform(X)
+
+std = StandardScaler()
+s = std.fit_transform(X)
+pca = PCA(n_components=30)
+pca.fit(s)
+pc = pca.transform(s)
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(pc)
+
+fr = pd.DataFrame({'tsne1': tsne[:,0], 'tsne2': tsne[:, 1], 'cluster': kmeans.labels_})
+
+
+y = train['SalePrice'].values
+def johnson(y):
+    gamma, eta, epsilon, lbda = st.johnsonsu.fit(y)
+    yt = gamma + eta*np.arcsinh((y-epsilon)/lbda)
+    return yt, gamma, eta, epsilon, lbda
+
+def johnson_inverse(y, gamma, eta, epsilon, lbda):
+    return lbda*np.sinh((y-gamma)/eta) + epsilon
+
+yt, g, et, ep, l = johnson(y)
+yt2 = johnson_inverse(yt, g, et, ep, l)
+
+
+def error(actual, predicted):
+    actual = np.log(actual)
+    predicted = np.log(predicted)
+    return np.sqrt(np.sum(np.square(actual-predicted))/len(actual))
+
+def log_transform(feature):
+    train[feature] = np.log1p(train[feature].values)
+    test[feature] = np.log1p(test[feature].values) #Transform values in 'test' data set alongside those in the 'train' data set
+
+def quadratic(feature):
+    train[feature+'2'] = train[feature]**2
+    test[feature+'2'] = test[feature]**2
+    
+log_transform('GrLivArea')
+log_transform('1stFlrSF')
+log_transform('2ndFlrSF')
+log_transform('TotalBsmtSF')
+log_transform('LotArea')
+log_transform('LotFrontage')
+log_transform('KitchenAbvGr')
+log_transform('GarageArea')
+
+quadratic('OverallQual')
+quadratic('YearBuilt')
+quadratic('YearRemodAdd')
+quadratic('TotalBsmtSF')
+quadratic('2ndFlrSF')
+quadratic('Neighborhood_E')
+quadratic('RoofMatl_E')
+quadratic('GrLivArea')
+
+qdr = ['OverallQual2', 'YearBuilt2', 'YearRemodAdd2', 'TotalBsmtSF2',
+        '2ndFlrSF2', 'Neighborhood_E2', 'RoofMatl_E2', 'GrLivArea2']
+
+train['HasBasement'] = train['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
+train['HasGarage'] = train['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
+train['Has2ndFloor'] = train['2ndFlrSF'].apply(lambda x: 1 if x > 0 else 0)
+train['HasMasVnr'] = train['MasVnrArea'].apply(lambda x: 1 if x > 0 else 0)
+train['HasWoodDeck'] = train['WoodDeckSF'].apply(lambda x: 1 if x > 0 else 0)
+train['HasPorch'] = train['OpenPorchSF'].apply(lambda x: 1 if x > 0 else 0)
+train['HasPool'] = train['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
+train['IsNew'] = train['YearBuilt'].apply(lambda x: 1 if x > 2000 else 0)
+
+test['HasBasement'] = test['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
+test['HasGarage'] = test['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
+test['Has2ndFloor'] = test['2ndFlrSF'].apply(lambda x: 1 if x > 0 else 0)
+test['HasMasVnr'] = test['MasVnrArea'].apply(lambda x: 1 if x > 0 else 0)
+test['HasWoodDeck'] = test['WoodDeckSF'].apply(lambda x: 1 if x > 0 else 0)
+test['HasPorch'] = test['OpenPorchSF'].apply(lambda x: 1 if x > 0 else 0)
+test['HasPool'] = test['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
+test['IsNew'] = test['YearBuilt'].apply(lambda x: 1 if x > 2000 else 0)
+
+boolean = ['HasBasement', 'HasGarage', 'Has2ndFloor', 'HasMasVnr', 'HasWoodDeck',
+            'HasPorch', 'HasPool', 'IsNew']
+
+
+features = quantitative + qual_encoded + boolean + qdr
+lasso = linear_model.LassoLarsCV(max_iter=10000,cv=3)
+houseFeats = train[features].fillna(0.).values
+housePrices = train['SalePrice'].values
+lasso.fit(houseFeats, np.log(housePrices))
+
+pricePred = np.exp(lasso.predict(houseFeats))
+error(housePrices, pricePred)
+
+
+import patsy
+
+housePrices, houseFeats = patsy.dmatrices(
+    "SalePrice ~ \
+        GarageCars + \
+        np.log1p(BsmtFinSF1) + \
+        ScreenPorch + \
+        Condition1_E + \
+        Condition2_E + \
+        WoodDeckSF + \
+        np.log1p(LotArea) + \
+        Foundation_E + \
+        MSZoning_E + \
+        MasVnrType_E + \
+        HouseStyle_E + \
+        Fireplaces + \
+        CentralAir_E + \
+        BsmtFullBath + \
+        EnclosedPorch + \
+        PavedDrive_E + \
+        ExterQual_E + \
+        bs(OverallCond, df=7, degree=1) + \
+        bs(MSSubClass, df=7, degree=1) + \
+        bs(LotArea, df=2, degree=1) + \
+        bs(FullBath, df=3, degree=1) + \
+        bs(HalfBath, df=2, degree=1) + \
+        bs(BsmtFullBath, df=3, degree=1) + \
+        bs(TotRmsAbvGrd, df=2, degree=1) + \
+        bs(LandSlope_E, df=2, degree=1) + \
+        bs(LotConfig_E, df=2, degree=1) + \
+        bs(SaleCondition_E, df=3, degree=1) + \
+        OverallQual + np.square(OverallQual) + \
+        GrLivArea + np.square(GrLivArea) + \
+        Q('1stFlrSF') + np.square(Q('1stFlrSF')) + \
+        Q('2ndFlrSF') + np.square(Q('2ndFlrSF')) +  \
+        TotalBsmtSF + np.square(TotalBsmtSF) +  \
+        KitchenAbvGr + np.square(KitchenAbvGr) +  \
+        YearBuilt + np.square(YearBuilt) + \
+        Neighborhood_E + np.square(Neighborhood_E) + \
+        Neighborhood_E:OverallQual + \
+        MSSubClass:BldgType_E + \
+        ExterQual_E:OverallQual + \
+        PoolArea:PoolQC_E + \
+        Fireplaces:FireplaceQu_E + \
+        OverallQual:KitchenQual_E + \
+        GarageQual_E:GarageCond + \
+        GarageArea:GarageCars + \
+        Q('1stFlrSF'):TotalBsmtSF + \
+        TotRmsAbvGrd:GrLivArea",
+    train.to_dict('list'))
+
+ridge = linear_model.RidgeCV(cv=10)
+ridge.fit(houseFeats, np.log(housePrices))
+pricePred = np.exp(ridge.predict(houseFeats))
+print(error(housePrices,pricePred))
+
+
+testHouseFeats = test[features].fillna(0.).values
+pricePred = np.exp(lasso.predict(testHouseFeats))
+with open ('test_predictions.csv','w',newline='') as csvfile:
+    writer = csv.writer(csvfile,delimiter=',',
+                        quotechar="'",quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(['Id', 'SalePrice'])
+    for i in range(1461,len(pricePred)+1461):
+        writer.writerow([i,pricePred[i-1461]])
+    
